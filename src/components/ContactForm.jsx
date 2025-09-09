95eab53a4878ce8e2393baaaaf3a37c1913f2f70
jonasdegroote1 <jonasdegroote5@gmail.com>
2025-09-09 19:08:13 +0200

    Update: form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,38 @@
+"use client";
+
+import { useState } from "react";
+
 export default function ContactForm() {
+  const [status, setStatus] = useState(""); // voor feedback
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // voorkom redirect
+    const form = e.target;
+
+    const data = new FormData(form);
+
+    try {
+      const response = await fetch("https://formspree.io/f/mqadnpln", {
+        method: "POST",
+        body: data,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (response.ok) {
+        setStatus("success");
+        form.reset();
+      } else {
+        setStatus("error");
+      }
+    } catch (err) {
+      setStatus("error");
+    }
+  };
+
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={handleSubmit}>
       <label htmlFor="name">Naam</label>
       <input
         type="text"
