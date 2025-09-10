524a5cb4a9ec537d7d8a959720c68097450bccd8
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2025-09-10 07:44:34 +0200

    Update DirectContact.jsx

--- src/components/DirectContact.jsx
+++ src/components/DirectContact.jsx
@@ -2,7 +2,11 @@ export default function DirectContact() {
   return (
     <div className="direct-contact">
       <h3>Direct Contact</h3>
-      <p>Email: <a href="mailto:jonasdegroote5@gmail.com">jonasdegroote5@gmail.com</a> </p>
+      <p>
+        <img 
+          src="/static/images/email.svg" 
+          alt="mail"
+          className="social-icon"/> <a href="mailto:jonasdegroote5@gmail.com">jonasdegroote5@gmail.com</a> </p>
       <p>GSM: +32 493 13 01 41 </p>
     </div>)
 }
