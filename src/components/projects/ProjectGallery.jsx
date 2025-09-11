60899807ca6828c12daddc74410f13727365924e
jonasdegroote1 <jonasdegroote5@gmail.com>
2025-09-11 19:06:40 +0200

    Add: Gallery

--- src/components/projects/ProjectGallery.jsx
+++ src/components/projects/ProjectGallery.jsx
@@ -1,13 +1,16 @@
-export default function ProjectGallery({ gallery, title }) {
-  if (!gallery?.length) return null;
+import "../../styles/components/ProjectGallery.css"
+
+
+export default function ProjectGallery({ project }) {
+  if (!project.gallery?.length) return null;
 
   return (
     <section className="project-gallery">
       <h2>Project Gallery</h2>
       <div className="gallery-grid">
-        {gallery.map((img) => (
+        {project.gallery.map((img) => (
           <div key={img.id} className="gallery-item">
-            <img src={img.url} alt={title} />
+            <img src={img.url} alt={project.title || "Project Image"} />
           </div>
         ))}
       </div>
