f445cb72f06bb9f34b7aee334ff123a777a93e0a
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2025-10-14 11:32:35 +0200

    Update Roof.jsx

--- src/components/3D/Roof.jsx
+++ src/components/3D/Roof.jsx
@@ -1,40 +1,37 @@
 "use client";
-import { useRef } from "react";
-import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 
 function Roof() {
-  const meshRef = useRef();
+  // Parameters
+  const width = 4;
+  const depth = 3;
+  const height = 2;
 
-  // dakvorm: driehoekig vlak (tweekantig)
-  const roofWidth = 4;
-  const roofDepth = 3;
-  const roofHeight = 2;
-
-  // driehoekig dakvlak voor linker- en rechterkant
-  const geometry = new THREE.BufferGeometry();
-  const vertices = new Float32Array([
-    -roofWidth / 2, 0, 0,       // linker onderhoek
-     roofWidth / 2, 0, 0,       // rechter onderhoek
-     0, roofHeight, roofDepth / 2, // top van het dak
-  ]);
-  geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
-  geometry.computeVertexNormals();
-
-  const material = new THREE.MeshStandardMaterial({ color: 0x8b0000, side: THREE.DoubleSide });
-
-  return <mesh ref={meshRef} geometry={geometry} material={material} position={[0,1.5,0]} />;
+  return (
+    <group position={[0, 1.5, 0]}>
+      {/* Linker dakvlak */}
+      <mesh rotation={[0, 0, Math.PI / 4]} position={[0, height / 2, 0]}>
+        <boxGeometry args={[width, 0.1, depth]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+      {/* Rechter dakvlak */}
+      <mesh rotation={[0, 0, -Math.PI / 4]} position={[0, height / 2, 0]}>
+        <boxGeometry args={[width, 0.1, depth]} />
+        <meshStandardMaterial color="darkred" />
+      </mesh>
+    </group>
+  );
 }
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [5,5,5], fov: 50 }}>
+    <Canvas camera={{ position: [6, 4, 6], fov: 50 }}>
       <ambientLight intensity={0.5} />
-      <directionalLight position={[5,10,5]} intensity={1} />
+      <directionalLight position={[5, 10, 5]} intensity={1} />
       <Roof />
       {/* Huisbasis */}
-      <mesh position={[0,0,0]}>
-        <boxGeometry args={[4,3,3]} />
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry args={[4, 3, 3]} />
         <meshStandardMaterial color="lightgray" />
       </mesh>
     </Canvas>
