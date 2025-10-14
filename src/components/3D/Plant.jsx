4675c9ed0dcecb09cdb1ed5e071f15b8a60c08fa
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2025-10-14 10:53:17 +0200

    Update Plant.jsx

--- src/components/3D/Plant.jsx
+++ src/components/3D/Plant.jsx
@@ -4,7 +4,7 @@ import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 
-function SinglePlant({ position = [0, 0, 0], scale = 0.5 }) {
+function SinglePlant({ position = [0, 0, 0], scale = 1 }) { // scale verhoogd
   const ref = useRef();
   const { scene } = useGLTF("/models/fancy-plant.glb");
 
@@ -35,13 +35,13 @@ function SinglePlant({ position = [0, 0, 0], scale = 0.5 }) {
 
 export default function PlantScene() {
   return (
-    <group>
-      {/* Cluster van planten zwevend */}
-      <SinglePlant position={[-1, 0.5, 0]} scale={0.5} />
-      <SinglePlant position={[1, 0.7, 0.5]} scale={0.6} />
-      <SinglePlant position={[0, 0.3, -1]} scale={0.4} />
-      <SinglePlant position={[0.8, 0.6, -0.8]} scale={0.45} />
-      <SinglePlant position={[-0.7, 0.4, 1]} scale={0.5} />
+    <group position={[0, -0.5, 0]}> {/* hele cluster iets naar beneden */}
+      {/* Cluster van planten */}
+      <SinglePlant position={[-0.5, 0, 0]} scale={1} />
+      <SinglePlant position={[0.5, 0.1, 0.3]} scale={1.1} />
+      <SinglePlant position={[0, -0.2, -0.5]} scale={0.9} />
+      <SinglePlant position={[0.6, 0, -0.4]} scale={0.95} />
+      <SinglePlant position={[-0.6, -0.1, 0.5]} scale={1} />
 
       {/* Magische zwevende bloemetjes/glows */}
       {[[-0.3, 1.0, 0.2, "pink"], [0.2, 0.9, -0.5, "lightblue"], [0.5, 0.8, 0.7, "lavender"]].map(
