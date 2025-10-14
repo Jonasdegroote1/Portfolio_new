8024a7ad6719a79dfe4eab01ea0d96261991fed6
Jonasdegroote1 <61138704+Jonasdegroote1@users.noreply.github.com>
2025-10-14 10:56:23 +0200

    Update Plant.jsx

--- src/components/3D/Plant.jsx
+++ src/components/3D/Plant.jsx
@@ -1,17 +1,21 @@
 "use client";
 import { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 
-function SinglePlant({ position = [0, 0, 0], scale = 1 }) { // scale verhoogd
+function SinglePlant({ position = [0, 0, 0], baseScale = 1 }) {
   const ref = useRef();
   const { scene } = useGLTF("/models/fancy-plant.glb");
+  const { size } = useThree();
+
+  // Schaal aanpassen voor mobiel
+  const scale = baseScale * (size.width < 600 ? 0.6 : 1); // kleiner op kleine schermen
 
   // Centraal positioneren via bounding box
   const box = new THREE.Box3().setFromObject(scene);
   const center = box.getCenter(new THREE.Vector3());
-  scene.position.sub(center); // verplaats model naar center
+  scene.position.sub(center);
 
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime();
@@ -35,15 +39,13 @@ function SinglePlant({ position = [0, 0, 0], scale = 1 }) { // scale verhoogd
 
 export default function PlantScene() {
   return (
-    <group position={[0, -0.5, 0]}> {/* hele cluster iets naar beneden */}
-      {/* Cluster van planten */}
-      <SinglePlant position={[-0.5, 0, 0]} scale={1} />
-      <SinglePlant position={[0.5, 0.1, 0.3]} scale={1.1} />
-      <SinglePlant position={[0, -0.2, -0.5]} scale={0.9} />
-      <SinglePlant position={[0.6, 0, -0.4]} scale={0.95} />
-      <SinglePlant position={[-0.6, -0.1, 0.5]} scale={1} />
+    <group position={[0, -0.7, 0]}>
+      <SinglePlant position={[-0.5, 0, 0]} baseScale={1} />
+      <SinglePlant position={[0.5, 0.1, 0.3]} baseScale={1.1} />
+      <SinglePlant position={[0, -0.2, -0.5]} baseScale={0.9} />
+      <SinglePlant position={[0.6, 0, -0.4]} baseScale={0.95} />
+      <SinglePlant position={[-0.6, -0.1, 0.5]} baseScale={1} />
 
-      {/* Magische zwevende bloemetjes/glows */}
       {[[-0.3, 1.0, 0.2, "pink"], [0.2, 0.9, -0.5, "lightblue"], [0.5, 0.8, 0.7, "lavender"]].map(
         ([x, y, z, color], i) => (
           <mesh key={i} position={[x, y, z]}>
@@ -57,7 +59,6 @@ export default function PlantScene() {
         )
       )}
 
-      {/* Lichtjes */}
       <pointLight position={[0, 2, 0]} intensity={0.6} color="white" />
       <ambientLight intensity={0.4} />
     </group>
